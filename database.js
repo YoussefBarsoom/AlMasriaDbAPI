58c4d56975e4ec8e3a0280b9b7e4d304830670bb
YoussefBarsoom <47917838+YoussefBarsoom@users.noreply.github.com>
2021-12-02 15:22:14 +0200

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('almasriadb', 'root', 'password', {
-    dialect: 'mysql',
-    host: 'localhost', 
-});
-
-export default sequelize;
